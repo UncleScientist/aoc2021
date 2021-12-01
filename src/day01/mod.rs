eb3a04ee2deecdfc0c0019f3f3172ff689b02321
Uncle Scientist <unclescientist@gmail.com>
2021-12-01 07:27:31 -0500

    day 1 part 1

--- src/day01/mod.rs
+++ src/day01/mod.rs
@@ -1,3 +1,20 @@
+use std::fs::File;
+use std::io::prelude::*;
+use std::io::BufReader;
+
 pub fn day01() {
-    println!("in day01");
+    let file = File::open("inputs/input-day01.txt").expect("Cannot find file");
+    let buf = BufReader::new(file);
+    let lines = buf.lines().map(|line| line.unwrap()).collect::<Vec<String>>();
+
+    let nums : Vec<i32> = lines.iter().map(|num| num.parse().unwrap()).collect();
+
+    let mut count = 0;
+    for i in nums.windows(2) {
+        if i[0] < i[1] {
+            count += 1;
+        }
+    }
+
+    println!("Day 01 - Part 1: {}", count);
 }
