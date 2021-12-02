e4a2774093239f2c9004ec611736b1d862efb6c1
Uncle Scientist <unclescientist@gmail.com>
2021-12-02 07:41:13 -0500

    day 2 part 2

--- src/day02/mod.rs
+++ src/day02/mod.rs
@@ -11,7 +11,7 @@ pub fn day02() {
         .collect::<Vec<String>>();
 
     println!("Day 02 - Part 1: {}", part1(&mut lines));
-    println!("Day 02 - Part 2: x");
+    println!("Day 02 - Part 2: {}", part2(&mut lines));
 }
 
 fn part1(commands: &mut Vec<String>) -> i32 {
@@ -27,6 +27,31 @@ fn part1(commands: &mut Vec<String>) -> i32 {
         .fold((0, 0), |a, b| (a.0 + b.0, a.1 + b.1));
 
     result.0 * result.1
+}
+
+fn part2(commands: &mut Vec<String>) -> i32 {
+    // (aim, hpos, depth)
+    //
+    let mut aim = 0;
+    let mut hpos = 0;
+    let mut depth = 0;
+
+    for cmd in commands {
+        let mut x = cmd.split_whitespace();
+        let dir = x.next().unwrap();
+        let val = x.next().unwrap().parse::<i32>().unwrap();
+        match dir {
+            "up" => aim -= val,
+            "down" => aim += val,
+            "forward" => {
+                hpos += val;
+                depth += aim * val;
+            }
+            x => panic!("got strange command: {}", x),
+        };
+    }
+
+    hpos * depth
 }
 
 #[cfg(test)]
@@ -45,5 +70,6 @@ mod tests {
         ];
 
         assert_eq!(part1(&mut commands), 150);
+        assert_eq!(part2(&mut commands), 900);
     }
 }
