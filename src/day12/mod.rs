55cb8809623f39d105b3f4435d2beb3ba64eddb7
Uncle Scientist <unclescientist@gmail.com>
2021-12-12 11:18:43 -0500

    day 12 part 2

--- src/day12/mod.rs
+++ src/day12/mod.rs
@@ -25,38 +25,71 @@ pub fn day12() {
     }
 
     println!("Day 12 - Part 1: {}", count_visited(&caves));
+    println!("Day 12 - Part 2: {}", count_double_visited(&caves));
 }
 
+#[derive(Default)]
 struct Pather {
-    visited: HashSet<String>,
+    visited: HashMap<String, usize>,
     path: Vec<String>,
+    unique: HashSet<String>,
 }
 
 fn count_visited(caves: &CavePath) -> usize {
     let mut path_walker = Pather {
-        visited: HashSet::new(),
-        path: Vec::new(),
+        ..Default::default()
     };
 
-    count_visited_from("start", &mut path_walker, caves)
+    count_visited_from("start", &mut path_walker, caves, &None)
 }
 
-fn count_visited_from(step: &str, walker: &mut Pather, caves: &CavePath) -> usize {
+fn count_double_visited(caves: &CavePath) -> usize {
+    let mut path_walker = Pather {
+        ..Default::default()
+    };
+
+    for c in caves.keys() {
+        if *c != "start" && *c != "end" {
+            let first = c.chars().next().unwrap();
+            if first.is_lowercase() {
+                count_visited_from("start", &mut path_walker, caves, &Some(c.to_string()));
+            } else {
+                count_visited_from("start", &mut path_walker, caves, &None);
+            }
+        }
+    }
+
+    path_walker.unique.len()
+}
+
+fn count_visited_from(
+    step: &str,
+    walker: &mut Pather,
+    caves: &CavePath,
+    double: &Option<String>,
+) -> usize {
     if step == "end" {
+        walker.unique.insert(format!("{:?}", walker.path));
         return 1;
     }
 
-    walker.visited.insert(step.to_string());
+    *walker.visited.entry(step.to_string()).or_default() += 1;
     walker.path.push(step.to_string());
 
     let mut count = 0;
     for next in caves.get(step).unwrap() {
-        if next.chars().next().unwrap().is_uppercase() || !walker.visited.contains(next) {
-            count += count_visited_from(next, walker, caves);
+        if next.chars().next().unwrap().is_uppercase()
+            || *walker.visited.entry(next.to_string()).or_default() == 0
+        {
+            count += count_visited_from(next, walker, caves, double);
+        } else if let Some(d) = double {
+            if d == next && *walker.visited.entry(next.to_string()).or_default() < 2 {
+                count += count_visited_from(next, walker, caves, double);
+            }
         }
     }
 
-    walker.visited.remove(step);
+    *walker.visited.entry(step.to_string()).or_default() -= 1;
     walker.path.pop();
 
     count
